941e5a31be7e0752a7a9e505f2920024fc01533f
Mardax <62964422+mardax007@users.noreply.github.com>
2022-07-12 16:09:06 +0200

    feat: :sparkles: added score download

---
+++ src/lib/scripts/downloadScore.ts
@@ -0,0 +1,19 @@
+import firebaseControlStore from "$lib/stores/firebaseControl";
+
+import type { FirebaseControl } from "$lib/scripts/interfaces";
+
+let firebaseControl: FirebaseControl;
+
+firebaseControlStore.subscribe(data => { 
+	firebaseControl = data;
+});
+
+
+
+export  default async function downloadScore(uid: string) {
+    const firestore = await import("firebase/firestore");
+    const doc = firestore.doc(firebaseControl.firestore, `songs/${uid}`);
+    const data = await firestore.getDoc(doc);
+    console.log(uid)
+    return data.data();
+}
--- src/routes/test/index.svelte
+++ src/routes/test/index.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
     import converter from "$lib/scripts/admin/converter";
-    import upload from "$lib/scripts/admin/upload";
+    import upload, { generateUID } from "$lib/scripts/admin/upload";
+    import downloadScore from "$lib/scripts/downloadScore";
 
     let convertResult: string;
     let uploadResult: string;
+    let generatedIDResult: string;
+    let downloadScoreResult: string;
 
     async function convertTest() {
-        convertResult = JSON.stringify(await converter(await (await fetch("https://firebasestorage.googleapis.com/v0/b/rollingsticksdev.appspot.com/o/media%2FTitleScore.mxl?alt=media&token=cac7bb4a-0bbd-4fbf-9e20-32a72619f193")).blob()))
+        const data = await converter(await (await fetch("https://firebasestorage.googleapis.com/v0/b/rollingsticksdev.appspot.com/o/media%2FTitleScore.mxl?alt=media&token=cac7bb4a-0bbd-4fbf-9e20-32a72619f193")).blob())
+        console.log(data)
+        convertResult = JSON.stringify(data)
     }
 
     async function UploadTest() {
-        uploadResult = JSON.stringify(await upload(await (await fetch("https://firebasestorage.googleapis.com/v0/b/rollingsticksdev.appspot.com/o/media%2FTitleScore.mxl?alt=media&token=cac7bb4a-0bbd-4fbf-9e20-32a72619f193")).blob()))
+        uploadResult = await upload(await (await fetch("https://firebasestorage.googleapis.com/v0/b/rollingsticksdev.appspot.com/o/media%2FTitleScore.mxl?alt=media&token=cac7bb4a-0bbd-4fbf-9e20-32a72619f193")).blob())
+        console.log(uploadResult)
+    }
+
+    async function GenerateIDTest() {
+        generatedIDResult = generateUID()
+        console.log(generatedIDResult)
+    }
+
+    async function downloadScoreTest() {
+        const data = await downloadScore("7997bb5e-d16a-444c-8b25-5406f25754f1")
+        console.log(data)
+        downloadScoreResult = JSON.stringify(data)
     }
 </script>
 
@@ -23,5 +40,15 @@
     <button id="UploadTest" on:click={UploadTest}>upload</button>
     {#if uploadResult}
         <p id="UploadTestOutput">{uploadResult}</p>
+    {/if}
+
+    <button id="GenerateIDTest" on:click={GenerateIDTest}>generate uid</button>
+    {#if generatedIDResult}
+        <p id="GenerateIDTestOutput">{generatedIDResult}</p>
+    {/if}
+
+    <button id="downloadScoreTest" on:click={downloadScoreTest}>download score</button>
+    {#if downloadScoreResult}
+        <p id="downloadScoreTestOutput">{downloadScoreResult}</p>
     {/if}
 </ul>
