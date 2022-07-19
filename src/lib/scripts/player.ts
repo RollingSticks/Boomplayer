60f42d19c402f40f5af0aab8b349434895e5cbbb
Mardax <62964422+mardax007@users.noreply.github.com>
2022-07-19 12:11:19 +0200

    fix: :bug: better title identification

--- src/lib/scripts/player.ts
+++ src/lib/scripts/player.ts
@@ -1,11 +1,24 @@
 import donwloadScore from './downloadScore';
 import type { Instrument, Measure, Score } from './interfaces';
 
+function identifyTitle(score: Score): string {
+    if (score['score-partwise'].work) {
+        return score['score-partwise'].work['work-title'];
+    } else if (score['score-partwise'].credit) {
+        score['score-partwise'].credit.forEach(credit => {
+            if (credit['credit-type'] === 'title') {
+                return credit['credit-words'];
+            }            
+        });
+    }
+    return 'Untitled';
+}
+
 export default async function getScore(url: string): Promise<Measure[]> {
     const score: Score = await donwloadScore(url);
     const parts: Measure[] = score["score-partwise"].part.measure ? [score["score-partwise"].part] : score["score-partwise"].part // it works, trust me
     const instruments: Instrument[] = score["score-partwise"]['part-list']['score-part'][0] ? score["score-partwise"]['part-list']['score-part'] : [score["score-partwise"]['part-list']['score-part']] // it works, trust me
-    const scoreName = score['score-partwise'].work ? score['score-partwise'].work['work-title'] : 'Untitled';
+    const scoreName = identifyTitle(score);
     
     
     
