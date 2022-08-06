852802a0f9cad2b79dcf07c45fa80965a9e1197b
Mardax <62964422+mardax007@users.noreply.github.com>
2022-08-06 14:46:49 +0200

    feat: :sparkles: seperate signin and signup pages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,5 @@
+<!-- Homepage -->
+
 <script lang="ts">
-    import type { AuthStore, Measure } from "$lib/scripts/interfaces";
-    import { signIn } from "$lib/scripts/auth";
-    import authStore from "$lib/stores/authStore";
-    import player from "$lib/scripts/player";
-
-    let data: Measure[]
-
-    async function downloadScore() {
-        data = await player("7997bb5e-d16a-444c-8b25-5406f25754f1")
-
-        data = await player("6803678b-0c9b-4855-88e8-7add912a96ff")
-    }
-
-    let AuthStoreData: AuthStore;
-
-    authStore.subscribe((data: AuthStore) => { 
-        AuthStoreData = data;
-    });
-
 </script>
-
-<button on:click={downloadScore}>Download</button>
-<p>Email</p>
-<input type="text" bind:value={AuthStoreData.userEmail} />
-<p>Password</p>
-<input type="text" bind:value={AuthStoreData.userPassword} />
-<button on:click={signIn}>Signin</button>
+<h1>Hello World</h1>
