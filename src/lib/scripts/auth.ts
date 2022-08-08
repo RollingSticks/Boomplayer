26d553de86b1c13b5c34c4fe4707b0fb12b1d3c9
Mardax <62964422+mardax007@users.noreply.github.com>
2022-08-08 20:22:59 +0200

    feat: :sparkles: what was I doing?

--- src/lib/scripts/auth.ts
+++ src/lib/scripts/auth.ts
@@ -3,7 +3,7 @@ import authStore from "$lib/stores/authStore";
 
 import type { FirebaseControl, AuthStore } from "$lib/scripts/interfaces";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile /* sendEmailVerification */ } from "firebase/auth";
-import { doc, updateDoc, arrayUnion } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 
 let firebaseControl: FirebaseControl;
 
@@ -28,7 +28,9 @@ async function signUp() { // TODO: add error handling
       displayName: AuthStoreData.displayName,
    });
 
-   updateDoc(doc(firebaseControl.firestore, `/users/metadata`), {userlist: arrayUnion(userInfo.user.uid)});
+   setDoc(doc(firebaseControl.firestore, `users/${userInfo.user.uid}`), {
+      songs: [],
+   })
 
    // sendEmailVerification(userInfo.user) // TODO: add email verification
 }
