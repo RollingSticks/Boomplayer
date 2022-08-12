414d7f46f246a684ed34356f0512967e620d6571
Mardax <62964422+mardax007@users.noreply.github.com>
2022-08-12 14:58:42 +0200

    fix: :bug: fix player

--- src/lib/scripts/player.ts
+++ src/lib/scripts/player.ts
@@ -1,23 +1,21 @@
-import donwloadScore from '$lib/scripts/downloadScore';
+import { downloadScore } from '$lib/scripts/downloadScore';
 import type { Instrument, Measure, Score } from '$lib/scripts/interfaces';
 import { identifyTitle, getParts, getInstruments } from '$lib/scripts/scoreInfoFinder';
 
 export default async function getScore(url: string): Promise<Measure[]> {
-    const score: Score = await donwloadScore(url);
+    const score: Score | undefined = await downloadScore(url);
+    if (!score) {
+        return [];
+    }
     const parts: Measure[] = getParts(score)
     const instruments: Instrument[] = getInstruments(score);
     const scoreName = identifyTitle(score);
-    
-    
-    
-    
-    
-    
-    console.log(instruments)
 
     for (let i = 0; i < parts.length; i++) {
         parts[i].instrument = instruments[i]
     }
+
+    console.log(parts)
 
     return parts
 }
