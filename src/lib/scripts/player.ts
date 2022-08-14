fcce3b4cc5c6ff4d74ae45797bbe373893bed7ba
Mardax <62964422+mardax007@users.noreply.github.com>
2022-08-14 15:25:14 +0200

    feat: :sparkles: implemented score in store in player

--- src/lib/scripts/player.ts
+++ src/lib/scripts/player.ts
@@ -1,14 +1,31 @@
 import { downloadScore } from "$lib/scripts/downloadScore";
-import type { Score } from "$lib/scripts/interfaces";
+import type { PlayerStore, Score } from "$lib/scripts/interfaces";
+import playerControl from "$lib/stores/playerControl";
+
+let playerControlStore: PlayerStore;
+
+playerControl.subscribe((data) => {
+	playerControlStore = data;
+});
 
 async function load(uid: string) {
 	const score: Score | undefined = await downloadScore(uid);
 	if (!score) {
 		// dispatch error
+		dispatchEvent(
+			new ErrorEvent("error", {
+				error: {
+					message: "Het nummer kon niet worden geladen",
+					retryable: true,
+					scoreUid: uid
+				}
+			})
+		);
+	} else {
+		playerControlStore.score = score;
+		playerControlStore.playing = false;
+		playerControlStore.bpm = score.bpm;
 	}
-
-	// save score to store
-	return score;
 }
 
 export { load };
