ec0d76398b49fb0fd5de94532b535fdd67459caa
Mardax <62964422+mardax007@users.noreply.github.com>
2022-08-14 15:08:28 +0200

    refactor: :fire: removed useless interpreter

--- src/lib/scripts/player.ts
+++ src/lib/scripts/player.ts
@@ -1,34 +1,14 @@
 import { downloadScore } from "$lib/scripts/downloadScore";
-import type {
-	RawInstrument,
-	RawMeasure,
-	ScoreRaw
-} from "$lib/scripts/interfaces";
-import {
-	identifyTitle,
-	getParts,
-	getInstruments
-} from "$lib/scripts/scoreInfoFinder";
+import type { Score } from "$lib/scripts/interfaces";
 
 async function load(uid: string) {
-	const score: ScoreRaw | undefined = await downloadScore(uid);
+	const score: Score | undefined = await downloadScore(uid);
 	if (!score) {
-		return null;
-	}
-	const parts: RawMeasure[] = getParts(score);
-	const instruments: RawInstrument[] = getInstruments(score);
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const scoreName = identifyTitle(score);
-
-	for (let i = 0; i < parts.length; i++) {
-		parts[i].instrument = instruments[i];
+		// dispatch error
 	}
 
+	// save score to store
 	return score;
 }
-
-// function play() {
-
-// }
 
 export { load };
