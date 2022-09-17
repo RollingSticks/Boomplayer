d0874114b8fb21a2237c6b330e50f678940df863
Mardax <{ID}+{username}@users.noreply.github.com>
2022-09-17 16:23:04 +0200

    docs: :construction: walking skeleton auth

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,21 @@
+<!-- Signup page -->
+<script lang="ts">
+	import type { AuthStore } from "$lib/scripts/interfaces";
+	import { signinWithGoogle, signUp } from "$lib/scripts/auth";
+	import authData from "$lib/stores/authData";
+
+	let AuthDataStore: AuthStore;
+
+	authData.subscribe((data: AuthStore) => {
+		AuthDataStore = data;
+	});
+</script>
+
+<p>Displayname</p>
+<input type="text" bind:value={AuthDataStore.displayName} />
+<p>Email</p>
+<input type="text" bind:value={AuthDataStore.userEmail} />
+<p>Password</p>
+<input type="text" bind:value={AuthDataStore.userPassword} />
+<button on:click={signUp}>Signup</button>
+<button on:click={signinWithGoogle}>Signup with Google</button>
