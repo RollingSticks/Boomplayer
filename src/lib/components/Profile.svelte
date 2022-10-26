67c89d7e68279e513055db9aee77a372fdf39829
Mardax <{ID}+{username}@users.noreply.github.com>
2022-10-26 11:24:46 +0200

    feat: :sparkles: signout button

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -50,13 +50,13 @@
 		left: 0;
 
 		margin-left: 4.85vw;
+		margin-top: 4.85vh;
 
 		#profilePicture {
 			// position: absolute;
 			height: 10vh;
 			width: 10vh;
 			border-radius: 50%;
-			margin-top: 5vh;
 			margin-left: 0.15vh;
 			cursor: pointer;
 		}
