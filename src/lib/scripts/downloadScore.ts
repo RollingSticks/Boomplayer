12ff575aa63261afc5da371f05a608362a24e3b1
Mardax <{ID}+{username}@users.noreply.github.com>
2022-10-27 21:37:29 +0200

    feat: :sparkles: caching songs

--- src/lib/scripts/downloadScore.ts
+++ src/lib/scripts/downloadScore.ts
@@ -10,25 +10,32 @@ firebaseControl.subscribe((data) => {
 
 async function downloadScore(uid: string): Promise<Score | undefined> {
 	try {
-		const firestore = await import("firebase/firestore");
-		const doc = firestore.doc(
-			firebaseControlStore.firestore,
-			`songs/${uid}`
-		);
-		const data = (await firestore.getDoc(doc)).data() ?? {};
-
-		if (!data.data) {
-			dispatchEvent(
-				new ErrorEvent("error", {
-					error: {
-						message: "Nummer bestaat niet meer",
-						songUid: uid,
-						retryable: true
-					}
-				})
+		let data = JSON.parse(localStorage.getItem(uid) || "{}");
+		if(!data.data || data.lastUpdated < Date.now() - 1000 * 60 * 60) {
+			const firestore = await import("firebase/firestore");
+			const doc = firestore.doc(
+				firebaseControlStore.firestore,
+				`songs/${uid}`
 			);
 
-			return undefined;
+			data = (await firestore.getDoc(doc)).data() ?? {};
+
+			data.lastUpdated = new Date().getTime();
+			localStorage.setItem(uid, JSON.stringify(data));
+
+			if (!data.data) {
+				dispatchEvent(
+					new ErrorEvent("error", {
+						error: {
+							message: "Nummer bestaat niet meer",
+							songUid: uid,
+							retryable: true
+						}
+					})
+				);
+
+				return undefined;
+			}
 		}
 		return data.data;
 	} catch (error) {
